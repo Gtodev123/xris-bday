377474be12c22b1e1e987ab38509bb5b20965d69
Gtodev123 <60323923+Gtodev123@users.noreply.github.com>
2025-08-14 20:38:10 +0300

    Xbday
    Xbday

--- Xris Bday/src/App.js
+++ Xris Bday/src/App.js
@@ -1,48 +1,16 @@
 
 import './App.css';
-import {useState , useEffect } from 'react'
-import Home from './Home'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route, Link,
-} from "react-router-dom";
-import Stash from './Stash';
-import button from './asets/button.png'
-import buttonStash from './asets/buttonStash.png'
+
+
 
 
 function App() {
 
-  const [isVisible, setIsVisible] = useState(false);
-  
-  useEffect(() => {
-    // Set isVisible to true after a delay
-    const timeout = setTimeout(() => {
-      setIsVisible(true);
-    }, 500); // Adjust the delay time (in milliseconds) as needed
-
-    // Clear the timeout to prevent memory leaks
-    return () => clearTimeout(timeout);
-  }, []);
-
-
   return (
-<Router>
     <div className='App'>
-      <div className='navbar'>
-      <Link className='link' to='/'><img src={button} alt='Home'/></Link>
-      <Link className='link' to='/stash'><img src={buttonStash} alt='Stash'/></Link>
-      </div>
-      
-    <div className={`main-container ${isVisible ? 'visible' : ''}`}>
+         
+  </div>
 
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-    </Routes>
-  </div>
-  </div>
-  </Router>
   );
 }
 
